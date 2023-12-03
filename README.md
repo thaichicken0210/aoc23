d5dec47aba1080c8c0c83a88bc8a4cdc5ac71925
Sarah C <thaichicken0210@gmail.com>
2023-12-03 16:43:32 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# aoc23
+# AoC 2023
+
+My attempts to solve the puzzles for the Advent of Code 2023. Hoo boy :)
  
--- aoc23.1.py
+++ aoc23.1.py
@@ -24,4 +24,28 @@ for j in range(1000):
 #     jk didn't need to do that bc i could build it in in line 21
 
 # sum up all the integers
-print(sum(calinteger))
+# print(sum(calinteger))
+
+# PART TWO
+
+#find and replace all text numbers with digits
+
+# find the new first and last digit
+calibration2 = []
+for i in range(1000):
+  test_string = input[i]
+  
+  numbers = []
+  for char in test_string:
+    if char.isdigit():
+     numbers.append(char)
+  calibration2.append([numbers[0],numbers[-1]])
+# print(calibration2)
+
+# concatenate and convert to integers
+calinteger2 = []
+for k in range(1000):
+  calinteger2.append(int(calibration[k][0]+calibration[k][1]))
+
+# sum up all the integers
+# print(sum(calinteger2))
