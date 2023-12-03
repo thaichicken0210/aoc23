86c74a059788f448ca9cbc7b4c5961605de94776
Sarah C <thaichicken0210@gmail.com>
2023-12-03 17:34:22 -0500

    Solve day 1.2
    replaced `range(1000)` in part 1 also with `range(len($listname))` in all instances

--- aoc23.1.py
+++ aoc23.1.py
@@ -5,7 +5,7 @@ with open("inputs\\aoc23.input1.txt") as f:
 # for each string in the calibration document
 # find the first and last digit
 calibration = []
-for i in range(1000):
+for i in range(len(input)):
   test_string = input[i]
   
   numbers = []
@@ -17,7 +17,7 @@ for i in range(1000):
 
 # concatenate those digits into new strings
 calinteger = []
-for j in range(1000):
+for j in range(len(calibration)):
   calinteger.append(int(calibration[j][0]+calibration[j][1]))
 
 # change the new strings to integers
@@ -29,11 +29,40 @@ for j in range(1000):
 # PART TWO
 
 #find and replace all text numbers with digits
+alldigits = []
+# mini = ['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen']
+# for m in range(len(mini)):
+#   newmini1 = mini[m].replace('one','o1ne')
+#   newmini2 = newmini1.replace('two','t2wo')
+#   newmini3 = newmini2.replace('three','t3hree')
+#   newmini4 = newmini3.replace('four','f4our')
+#   newmini5 = newmini4.replace('five','f5ive')
+#   newmini6 = newmini5.replace('six', 's6ix')
+#   newmini7 = newmini6.replace('seven','s7even')
+#   newmini8 = newmini7.replace('eight','e8ight')
+#   newmini9 = newmini8.replace('nine','n9ine')
+#   alldigits.append(newmini9)
+
+for m in range(len(input)):
+  newinput1 = input[m].replace('one','o1ne')
+  newinput2 = newinput1.replace('two','t2wo')
+  newinput3 = newinput2.replace('three','t3hree')
+  newinput4 = newinput3.replace('four','f4our')
+  newinput5 = newinput4.replace('five','f5ive')
+  newinput6 = newinput5.replace('six', 's6ix')
+  newinput7 = newinput6.replace('seven','s7even')
+  newinput8 = newinput7.replace('eight','e8ight')
+  newinput9 = newinput8.replace('nine','n9ine')
+  alldigits.append(newinput9)
+
+# print(alldigits)
+
+
 
 # find the new first and last digit
 calibration2 = []
-for i in range(1000):
-  test_string = input[i]
+for i in range(len(alldigits)):
+  test_string = alldigits[i]
   
   numbers = []
   for char in test_string:
@@ -44,8 +73,8 @@ for i in range(1000):
 
 # concatenate and convert to integers
 calinteger2 = []
-for k in range(1000):
-  calinteger2.append(int(calibration[k][0]+calibration[k][1]))
+for k in range(len(calibration2)):
+  calinteger2.append(int(calibration2[k][0]+calibration2[k][1]))
 
 # sum up all the integers
-# print(sum(calinteger2))
+print(sum(calinteger2))
